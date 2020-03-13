4b0a4a0a33e8ef44e3d8582d5569fce27c35951e
Youngmuk Kang <9507ym@naver.com>
2020-03-13 19:28:23 +0900

    STL study

--- Template/Template/Template.cpp
+++ Template/Template/Template.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 // Template : 똑같은 절차가 있을때 데이터 타입이 다른 경우 사용하는 문법 >>>> 제네릭같은거
 
-template <typename T>	// 여기서 T 는 타입이름을 뜻함 ----> int , float , char 같은것들..
-T sum(T a, T b)			// T는 
+template <typename T = int>	// 여기서 T 는 타입이름을 뜻함 ----> int , float , char 같은것들.. // 기본값으로는 int로 처리
+T sum(T a, T b)				// T는 
 {
 	return a + b;
 }
@@ -16,6 +16,15 @@ void Swap(T &a, T&b)
 	T empty = a;
 	a = b;
 	b = empty;
+}
+
+template <> 
+void Swap<float>(float &a, float&b) // Swap 함수에서 특정한 타입은 특수하게 적용하고 싶을때 특수화를 사용하면된다!!
+{
+	float empty = a;
+	a = b;
+	b = empty;
+	cout << a << endl;
 }
 
 int main()
